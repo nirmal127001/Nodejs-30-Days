1d9842be5d1c239e14a470a1548aee59938329c7
nirmal <nirmalkr805@gmail.com>
2024-02-05 00:00:37 +0530

    jhgdf

--- tutorial/fs.js
+++ tutorial/fs.js
@@ -5,4 +5,13 @@ console.log("content: "+content)
 
 // fs.writeFileSync('../Files/file.txt',"This is new message from write function") //overrides with new data wiping previous data or creates this file if not exist
 //fs.appendFileSync('../Files/file.txt'," This is appended message")
-fs.unlinkSync('../Files/temp.txt')
+// fs.unlinkSync('../Files/temp.txt') //to delete a file
+
+let path = "../Files"
+FolderContent = fs.readdirSync(path) 
+console.log(FolderContent)
+
+let flag = fs.existsSync('Files') //to check if dir/files exist
+console.log(flag)
+
+fs.rmdirSync('folder') //to delete empty dir called folder
