3bc9d5f7ef1b30a714dc2d742eb789b1a1ea7c42
nirmal <nirmalkr805@gmail.com>
2024-02-14 21:44:03 +0530

    Problem 8

--- problems/8ExpressErrorHandling.js
+++ problems/8ExpressErrorHandling.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const app = express()
 
-function positiveIntegerHandler(req, res) {
-    
+function positiveIntegerHandler(req, res, next) {
+    const error = new Error('This is negative number!')
+    console.log(parseInt(req.params.number))
+    if(req.params.number <0){
+      console.log(error)
+    }
+    next()
   }
 
 app.use(positiveIntegerHandler)
 
-app.get('/',(req,res)=>{
-    res.send('Hello')
+app.get('/positive/:number',(req,res)=>{
+    res.send('Success')
 })
 
 const port = process.env.PORT || 8080  //creates dynamic port or default 8080
