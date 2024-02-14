d2d2407f89535430a7f23b23873644216586a34e
nirmal <nirmalkr805@gmail.com>
2024-02-14 23:22:58 +0530

    Problem 8

--- problems/8ExpressErrorHandling.js
+++ problems/8ExpressErrorHandling.js
@@ -3,16 +3,17 @@ const app = express()
 
 function positiveIntegerHandler(req, res, next) {
     const error = new Error('This is negative number!')
-    console.log(parseInt(req.params.number))
-    if(req.params.number <0){
-      console.log(error)
+    const number=parseInt(req.query.number)
+    if(number <0){
+      // console.log(error.message)
+      res.send(error.message)
     }
-    next()
+    else next()
   }
 
 app.use(positiveIntegerHandler)
 
-app.get('/positive/:number',(req,res)=>{
+app.get('/positive',(req,res)=>{
     res.send('Success')
 })
 
